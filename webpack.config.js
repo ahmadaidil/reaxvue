46c65feb7bf6d937156124a9bd97409169b00031
Ahmad Aidil F <ahmdaidil@gmail.com>
2018-05-12 18:23:47 +0700

    babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,18 @@ module.exports = {
     publicPath: '/',
     filename: '[name].bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
   devServer: {
     contentBase: './dist'
   }
