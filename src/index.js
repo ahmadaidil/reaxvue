e024b51bdf309cc252182d490bee2e928321b36c
Ahmad Aidil F <ahmdaidil@gmail.com>
2018-05-13 03:16:18 +0700

    change jsx to js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Routes from './routes';
 
 render(
-  <div>Pee ka Boo !</div>,
+  <Routes />,
   document.getElementById('app')
 );
